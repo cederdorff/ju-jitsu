d73c5ac139fe3b0e773c57b70fec013b1bc8f6e1
ChaEma7 <70863947+ChaEma7@users.noreply.github.com>
2021-12-09 20:36:11 +0100

    09-12-2021

--- js/app.js
+++ js/app.js
@@ -1,120 +1,31 @@
 import { navigateTo } from "./router.js";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-app.js";
-import {
-	getAuth,
-	onAuthStateChanged,
-	signOut
-} from "https://www.gstatic.com/firebasejs/9.4.1/firebase-auth.js";
 
-import {
-    getFirestore,
-    collection
-} from "https://www.gstatic.com/firebasejs/9.4.1/firebase-firestore.js";
-
-
-// ========== FIREBASE SIGN IN FUNCTIONALITY ========== //
-// Your web app's Firebase configuration
-const firebaseConfig = {
-	apiKey: "AIzaSyCCcbowFE6WkMAZJrrOwwzLEx4xLmdGJMY",
-	authDomain: "hjjn-edd00.firebaseapp.com",
-	projectId: "hjjn-edd00",
-	storageBucket: "hjjn-edd00.appspot.com",
-	messagingSenderId: "792163728984",
-	appId: "1:792163728984:web:7805af5d294f4346a6542d"
-};
-// Initialize Firebase (modular)
-initializeApp(firebaseConfig);
-const _auth = getAuth();
-
-// Initialize Firebase UI (non modular)
-firebase.initializeApp(firebaseConfig);
-let _firebaseUI;
-
-// ========== FIREBASE AUTH ========== //
-// Listen on authentication state change
-
-onAuthStateChanged(_auth, user => {
-	console.log(user);
-	if (user) {
-		userAuthenticated(user);
-	} else {
-		// User is signed out
-		userNotAuthenticated();
-		if (location.hash === "#/medlemmer" || location.hash === "#/login") {
-			navigateTo("#/login");
-		}
-	}
-});
-
-function userAuthenticated(user) {
-	appendUserData(user);
-	navigateTo("#/medlemmer");
-}
-
-function appendUserData(user) {
-	console.log(user);
-}
-
-function userNotAuthenticated() {
-
-	// Firebase UI configuration
-	const uiConfig = {
-		credentialHelper: firebaseui.auth.CredentialHelper.NONE,
-		signInOptions: [
-			firebase.auth.EmailAuthProvider.PROVIDER_ID
-		],
-		signInSuccessUrl: "#/medlemmer"
-	};
-	// Init Firebase UI Authentication
-	if (!_firebaseUI) {
-		_firebaseUI = new firebaseui.auth.AuthUI(firebase.auth());
-	}
-	_firebaseUI.start("#firebaseui-auth-container", uiConfig);
-	//showLoader(false);
-}
-
-function logout() {
-	signOut(_auth);
-}
-
-// ========== Reading from collection (modular v9) ========== //
-
-// reference to darabase
-const _db = getFirestore();
-// reference to users collection in database
-const _usersRef = collection(_db, "users");
-let _users = [];
-
-
-// =========== attach events =========== //
-document.querySelector("#btn-logout").onclick = () => logout();
 window.navigateTo = path => navigateTo(path);
 
 // =================================================== Navigation =====================================================
 //adgang til hamburger og nav-menu class
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const subscribeBtn =  document.querySelector(".lil-button");
 
 //add en event lisenter til hamburger
 hamburger.addEventListener("click", () => {
-	//activer classen der ændre min bar til et X og den class der viser menuen
+	//activer classen der ændre mine bar til et X og den class der viser menuen
 	hamburger.classList.toggle("active");
 	navMenu.classList.toggle("active");
-	
+	subscribeBtn.classList.toggle("hide");
 });
 
 //Søger for at nav lukker sig og bliver "normal" når man klikker på et link.
-document.querySelectorAll(".nav-link").forEach(n =>
+document.querySelectorAll(".nav-link").forEach(link =>
 	 //add en eventlistener
-		n.addEventListener("click", () => {
+		link.addEventListener("click", () => {
 			//click event, der vil fjerne active class
 			hamburger.classList.remove("active");
 			navMenu.classList.remove("active");
+			subscribeBtn.classList.add("hide");
 		})
 );
-
-
-
 
 
 // ===========  hide og show knap til pensum på "for medlemmer" ===========  //
@@ -142,15 +53,11 @@ document.querySelector("#btn-adult").onclick = () => btn_adult();
 document.querySelector("#btn-children").onclick = () => btn_children();
 
 // =  hide og show knap til pensum børne pensum på "for medlemmer" =  //
-function btn_belt_children() {
-	$("#belt_childrent_hide").toggleClass("open");
-	if (!$("#belt_childrent_hide").hasClass("open")) {
-		$(".btn_belt_children").text("... Se flere");
-	} else {
-		$(".btn_belt_children").text("... Se mindre");
-	}
-};
-
-document.querySelector("#belt_childrent_hide").onclick = () => btn_belt_children();
-
-
+// function btn_belt_children.click(() =>{
+// 	$(".belt_childrent_hide").toggleClass("open");
+// 	if (!$(".belt_childrent_hide").hasClass("open")) {
+// 		$(".btn_belt_children").text("... Se flere");
+// 	} else {
+// 		$(".btn_belt_children").text("... Se mindre");
+// 	}
+// });
--- js/router.js
+++ js/router.js
@@ -1,3 +1,106 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-app.js";
+import {
+	getAuth,
+	onAuthStateChanged,
+	signOut
+} from "https://www.gstatic.com/firebasejs/9.4.1/firebase-auth.js";
+
+import {
+    getFirestore,
+    collection
+} from "https://www.gstatic.com/firebasejs/9.4.1/firebase-firestore.js";
+
+
+// ========== FIREBASE SIGN IN FUNCTIONALITY ========== //
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyCCcbowFE6WkMAZJrrOwwzLEx4xLmdGJMY",
+	authDomain: "hjjn-edd00.firebaseapp.com",
+	projectId: "hjjn-edd00",
+	storageBucket: "hjjn-edd00.appspot.com",
+	messagingSenderId: "792163728984",
+	appId: "1:792163728984:web:7805af5d294f4346a6542d"
+};
+// Initialize Firebase (modular)
+initializeApp(firebaseConfig);
+const _auth = getAuth();
+
+// Initialize Firebase UI (non modular)
+firebase.initializeApp(firebaseConfig);
+let _firebaseUI;
+
+// ========== FIREBASE AUTH ========== //
+// Listen on authentication state change
+
+onAuthStateChanged(_auth, user => {
+	console.log(user);
+	if (user) {
+		userAuthenticated(user);
+	} else {
+		// User is signed out
+		userNotAuthenticated();
+		if (location.hash === "#/medlemmer" || location.hash === "#/login") {
+			navigateTo("#/login");
+		}
+	}
+});
+
+function userAuthenticated(user) {
+	appendUserData(user);
+	navigateTo("#/medlemmer");
+}
+
+function appendUserData(user) {
+	console.log(user);
+}
+
+function userNotAuthenticated() {
+
+	// Firebase UI configuration
+	const uiConfig = {
+		credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+		signInOptions: [
+			firebase.auth.EmailAuthProvider.PROVIDER_ID
+		],
+		signInSuccessUrl: "#/medlemmer"
+	};
+	// Init Firebase UI Authentication
+	if (!_firebaseUI) {
+		_firebaseUI = new firebaseui.auth.AuthUI(firebase.auth());
+	}
+	_firebaseUI.start("#firebaseui-auth-container", uiConfig);
+	//showLoader(false);
+}
+
+function logout() {
+	signOut(_auth);
+	navigateTo("#/");
+	scrollToTop();
+}
+
+function scrollToTop() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
+
+
+// =========== attach events =========== //
+document.querySelector("#btn-logout").onclick = () => logout();
+
+// ========== Reading from collection (modular v9) ========== //
+
+// >>>>>>>>> dette bruger I ikke, gør I?
+
+// // reference to darabase
+// const _db = getFirestore();
+// // reference to users collection in database
+// const _usersRef = collection(_db, "users");
+// let _users = [];
+
+
+// ========== Router STUFF ========== //
+
 /**
  * All routes of the SPA
  * "path": "id of page in DOM"
@@ -33,13 +136,13 @@ export function navigateTo(path) {
  * Displaying page by given path
  */
 function showPage(path) {
+	if(path === "#/medlemmer" && !_auth.currentUser){
+		path ="#/login"
+	}
+
 	hideAllPages(); // hide all pages
-	console.log(_routes[path]);
 	document.querySelector(`#${_routes[path]}`).style.display = "block"; // show page by given path
-	if (path == "#/login" || path == "#/signup") {
-		// showTabbar(false);
-	} else {
-		// showTabbar(true);
+	if (path != "#/login" && path != "#/signup") {
 		setActiveTab(path);
 	}
 }
@@ -96,4 +199,3 @@ function showTabbar(show) {
 		tabbar.classList.add("hide");
 	}
 }
-
